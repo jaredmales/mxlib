9d0467981a51d6e9745a63ea647ba24afb293c98
Jared Males <jaredmales@gmail.com>
2015-07-28 15:28:04 -0700

    ODI negative angle, now positive

--- include/ADIobservation.hpp
+++ include/ADIobservation.hpp
@@ -105,7 +105,7 @@ struct derotODI
    ///Calculate the derotation angle for a given image number
    floatT derotAngle(size_t imno) const
    {
-      return -1*(fmod(dateobs[imno], period)/period)*D2PI;
+      return D2PI-(fmod(dateobs[imno], period)/period)*D2PI;
    }
 };
 
