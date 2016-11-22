d78161f7a05b9a87804d3482918f3708457c7bc6
Jared <jaredmales@gmail.com>
2016-11-22 10:39:45 -0300

    more work on gnuPlot

--- include/gnuPlot.hpp
+++ include/gnuPlot.hpp
@@ -14,14 +14,17 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <poll.h>
 
 #include <stdlib.h>
 
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 #include <cmath>
+
 
 /** \addtogroup plotting
   * @{
@@ -30,9 +33,21 @@
 namespace mx
 {
    
-#define MX_GP_FNAME_SZ 64
+#ifndef MX_GP_FNAME_SZ
+///The size of the string for managing the stderr fifo
+#define MX_GP_FNAME_SZ 128
+#endif
+   
+#ifndef MX_GP_TEMP_SZ
+///The size of the string for managing temporary file names
 #define MX_GP_TEMP_SZ 128
+#endif
 
+#ifndef MX_GP_FC_TIME
+///Time in microseconds to wait for gnuplot startup and file creation to complete.
+#define MX_GP_FC_TIME 10000
+#endif
+   
 //Get the gnuplot binary format string for a type
 //Specializations below.
 template<typename dataT>
@@ -42,16 +57,67 @@ std::string gpBinaryFormat()
    return "";
 }
 
+double gp_get_curr_time()
+{
+   struct timespec tsp;
+   clock_gettime(CLOCK_REALTIME, &tsp);
+   
+   return ((double)tsp.tv_sec) + ((double)tsp.tv_nsec)/1e9;
+}
 
 
 /// A c++ interface to gnuplot
 /** Spawns a gnuplot sesssion and communicates with it.
+  * \ingroup plotting 
+  * 
+  * \code
+  * gnuPlot gp; //This automatically connects, and is now ready to plot.
+  * 
+  * gp.plot("my_data_file.dat", "u 1:2 w l"); //Plots the data in the file
+  * 
+  * std::cout << gp.getResponse("show terminal") << "\n"; //Sends the command to gnuplot, gets the response.
+  * \endcode
+  * 
+  * Error checking and reporting is not straightforward since gnuplot does not return a result if there is no error.  If there is an error,
+  * it can take several hundred milliseconds for a response to be available.  So for a typical 
+  *  plot command, say, one does not want to try to read the stderr ouput just in case as this will just time out with a delay.
+  *
+  * For cases where a response is expected, the following example shows how to check for errors.
+  * \code
+  * errno = 0;
+  * std::string response = gp.getResponse("show terminal"); //Waits up to 0.5 seconds, or a user specified timeout. 
+  * 
+  * if(response == "") //This indicates some error occurred, or a timeout.
+  * {
+  *   if(gp.gpError())
+  *   {
+  *      std::cerr << "gnuplot returned error:\n" << gp.gpErrorMsg() << "\n";
+  *   }
+  *   else if(errno)
+  *   {
+  *      perror("error getting response: ");
+  *   }
+  *   else
+  *   {
+  *      std::cerr << "timed out\n";
+  *   }
+  * }
+  * else
+  * {
+  *   std::cout << response << "\n";
+  * }
+  * 
+  * \endcode
   */
 class gnuPlot
 {
 protected:   
+
+   int _connected;
    
-   int _initialized;
+   ///Set to true if the response indicates a gnuplot error
+   bool _gpError;
+   std::string _gpErrorMsg;
    
    ///File stream for the gnuplot interface
    FILE * _pipeH;
@@ -62,7 +128,7 @@ protected:
    std::string _errLocation;
    
    ///File name of the gnuplot stderr fifo
-   char _errFName[MX_GP_FNAME_SZ];
+   std::string _errFName;
    
    ///File descriptor for the gnuplot stderr fifo
    int _errFD;
@@ -83,8 +149,29 @@ public:
    gnuPlot();
    
    ~gnuPlot();
+
+   ///Connect to gnuplot
+   /** Spawns a gnuplot session using popen with stderr redirected to a temporary file.  The temporary file
+     * is opened for reading.
+     *
+     * \retval 0 on success
+     * \retval -1 on error
+     */ 
+   int connect();
    
-   void init();
+   ///Return the value of the gpError flag.
+   /** This flag is set if gnuplot returns an error message (see checkResponse()).  The error message
+     * can be accessed with gpErrorMsg(). 
+     *
+     * \retval 0 if no error
+     * \retval -1 if an error has occurred. 
+     */
+   bool gpError();
+   
+   ///Return the gnuplot error message
+   /** The error message is extracted by checkResponse() when the response from gnuplot begins with "gnuplot>".
+     */
+   std::string gpErrorMsg();
    
    ///Send a command to gnuplot
    /** The newline is appended to the command, and then it is sent to gnuplot.
@@ -96,6 +183,36 @@ public:
      * \retval -1 on error
      */ 
    int command(const std::string & com, bool flush = true);
+     
+   ///Check for a response from gnuplot.
+   /** It typically takes some time for a response from gnuplot to become readable,
+     * whether an error occurs or not. It is often 0.3 seconds or more.  If there is no error, there will be no response so waiting
+     * for a response to check for errors after every command can be very time consuming.  Thus it is a choice whether to check 
+     * for errors from gnuplot after every command, and what timeout to use. 
+     * 
+     * gnuplot terminates all outputs with \c \\n\\n, so this reads up to these two characters.  It then strips any leading and trailing whitespace. 
+     * 
+     * If the response begins with \c gnuplot>, then the response is an error.  In this case, checkResponse returns -1 and the gpError flag is set.  If gpError is not set 
+     * but the return value is -1, then some other error occurred (check \c errno).
+     * 
+     * \param response contains the response from gnuplot, but is empty on timeout or error.
+     * \param timeout [optional] is the length of time, in seconds, to wait for a response. Default is 0, but a minimum of 0.5 if a response is expected.
+     * 
+     * \retval 0 on timeout or successful read
+     * \retval -1 on error, or if the gnuplot response indicates an error.
+     */
+   int checkResponse(std::string & response, double timeout = 0);
+   
+   ///Get a response from gnuplot for a given command.
+   /** This should only be used if a response is expected.
+     *
+     * \param com is the command string
+     * \param timeout [optional] is the length of time, in seconds, to wait for a response. Default is 0.5, which is the minimum that should be used.
+     * 
+     * \returns "" (empty string) on timeout or error (\c errno and gpError() should be checked), and the response from gnuplot otherwise.
+     */ 
+   std::string getResponse( const std::string & com, double timeout=0.5);
+
    
    /// Issue the \b replot command
    /**
@@ -266,10 +383,14 @@ public:
    
 protected:
    
+      
    ///Open a temporary binary file, and provide the filename. 
    FILE * openTempFile(char * fname);
    
 
+private:
+   ///Initialize an instance, only used at construction
+   void init();
    
    
 };
@@ -278,17 +399,20 @@ protected:
 inline
 gnuPlot::gnuPlot()
 {
-   _initialized = 0;
+   init();
+}
+
+inline
+void gnuPlot::init()
+{
+   _connected = 0;
 
    _pipeH = 0;
 
    _errLocation = "/dev/shm";
-   _errFName[0] = 0;
    _errFD = 0;
 
    _tempLocation = "/dev/shm";
-   
-   init();
 }
 
 inline
@@ -304,9 +428,9 @@ gnuPlot::~gnuPlot()
       close(_errFD);
    }
    
-   if(_errFName[0] > 0)
+   if(_errFName.length() > 0)
    {
-      remove(_errFName);
+      remove(_errFName.c_str());
    }
    
    for(int i=0; i < _tempFiles.size(); ++i)
@@ -317,72 +441,174 @@ gnuPlot::~gnuPlot()
 }
 
 inline
-void gnuPlot::init()
+int gnuPlot::connect()
 {
 
-   if(_initialized) return;
+   if(_connected) return 0;
+
+   srand( (unsigned long)  this);  //We don't need random, just different from all other instances in this process
+                                   
    
-   snprintf(_errFName, MX_GP_FNAME_SZ, "%s/gperr%d", _errLocation.c_str(), getpid());
+   char tmpStr[MX_GP_FNAME_SZ];
+
+   snprintf(tmpStr, MX_GP_FNAME_SZ, "/gperr%d_%d", getpid(), rand());
+
+   _errFName = _errLocation + tmpStr;
    
-   if (mkfifo(_errFName, 0600)) 
+   
+   std::string comm = "gnuplot -persist 2>";
+   comm += _errFName;
+   
+   
+   _pipeH = popen(comm.c_str(), "w");
+
+   usleep(MX_GP_FC_TIME);//allow file creation to finish before opening stderr file
+   
+   errno = 0;
+   _errFD = open(_errFName.c_str(), O_RDONLY);
+   if(_errFD <= 0)
    {
-      if (errno != EEXIST) 
-      {
-         fprintf(stderr, "could not create gnuplot stderr fifo\n");
-      }
-   }
-   else
-   {
-      _errFD = open(_errFName, O_RDONLY  | O_NONBLOCK); 
+      perror("gnuPlot failed to open file: ");
+      return -1;
    }
    
-   char comm[2*MX_GP_FNAME_SZ];
-   snprintf(comm, 2*MX_GP_FNAME_SZ, "gnuplot -persist 2>%s", _errFName);
-   
-   _pipeH = popen(comm, "w");
-   
-   _initialized = 1;
+   _connected = 1;
 }
 
+inline
+bool gnuPlot::gpError()
+{
+   return _gpError;
+}
+
+inline
+std::string gnuPlot::gpErrorMsg()
+{
+   return _gpErrorMsg;
+}
+   
 inline
 int gnuPlot::command(const std::string & com, bool flush)
 {
    
-   if(!_initialized) init();
+   if(!_connected) connect();
    
    fprintf(_pipeH, "%s", (com + "\n").c_str());
    if(flush) fflush(_pipeH);
-   usleep(1e6/10); //give time for response
-         
+   
+   return 0;
+     
+
+}
+
+inline
+int gnuPlot::checkResponse(std::string & response, double timeout)
+{
+   _gpError = false;
    if(_errFD)
    {
       char errstr[1024];
-      int rv = read(_errFD, errstr, 1024);
-   
-      if(rv < 0 && !( errno == EAGAIN || errno == EWOULDBLOCK))
+      int rv = 0;
+      
+      double t0 = gp_get_curr_time();
+
+      errno = 0;
+      rv = read(_errFD, errstr, 1024);
+      
+      while(rv == 0 && gp_get_curr_time() - t0 <= timeout)
       {
-         perror("Could not read gnuplot stderr");
-         //std::cerr << "Could not read gnuplot stderr\n";
-         return 1;
+         usleep(10); //Give up the thread
+         rv = read(_errFD, errstr, 1024);
+      }
+      
+      if(rv < 0)
+      {
+         perror("gnuPlot: ");
+         response = "";
+         return -1;
+      }
+      
+      if(rv == 0)
+      {
+         std::cerr << "timed out";
+         response = "";
+         return 0;
       }
       
       errstr[rv] = '\0';
       
-      if(rv > 0) 
+      response = errstr;
+      
+      bool done = false;
+      if(response.length() > 1)
       {
-         std::cerr << "gnuplot error\n";
-         std::cerr << errstr << "\n";
-         
-         return -1;
+         if(response[response.length()-1] == '\n' && response[response.length()-2] == '\n') done = true;
       }
+      
+      
+      while( !done && gp_get_curr_time() - t0 < timeout)
+      {
+         usleep(10); //Give up the thread
+         rv = read(_errFD, errstr, 1024);
+         
+         if(rv > 0)
+         {
+            errstr[rv] = '\0';
+            response += errstr;
+       
+            if(response.length() > 1)
+            {
+               if(response[response.length()-1] == '\n' && response[response.length()-2] == '\n') done = true;
+            }
+         }
+      }
+
+      int first =0;
+      while( isspace(response[first]) && first < response.length()-1) ++first;
+      response.erase(0, first);
+      
+      
+      int last = response.length()-1;
+      while( response[last] == '\n' && last > 0) --last;
+      if(response[last] != '\n') ++last;
+      
+      response.erase(last);
+      
+      
+      if(response.length() > 8)
+      {
+         if(response.substr(0,8) == "gnuplot>")
+         {
+            _gpError = true;
+            _gpErrorMsg = response;
+            response = "";
+            return -1;
+         }
+      }
+      
+      
       return 0;
    }
    
    //If errFD is not open for some reason
    std::cerr << "gnuplot stderr is not open\n";
    return 1;
+   
+   
 }
 
+inline
+std::string gnuPlot::getResponse( const std::string & com, double timeout)
+{
+   std::string response;
+      
+   if(command(com, true) < 0) return response;
+   
+   checkResponse(response, timeout);
+   
+   return response;
+}
+   
 inline
 int gnuPlot::replot()
 {
