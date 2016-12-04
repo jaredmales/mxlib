2cc74ffca5c1aa9b13956fe1090d8187e98ab638
Jared <jaredmales@gmail.com>
2016-12-03 17:14:10 -0700

    made gengithead.sh center text

--- gengithead.sh
+++ gengithead.sh
@@ -4,6 +4,17 @@
 #$2 = optional ouput path, default ./git_version.h
 #$3 = optional prefix, default is GIT 
 
+
+centerName(){
+  textsize=${#1}
+  width=42
+  span=$((($width + $textsize) / 2 ))
+  espan=$(($span - $textsize))
+  if [ $(($espan % 2)) -eq 1 ]; then espan=$(($espan - 1)); fi 
+  printf "    #pragma message (\"*%${span}s%${espan}s\")\n" "$1" "*"
+}
+
+
 if [ "$1" != "" ]; then
     GITPATH=$1
 else
@@ -13,7 +24,7 @@ fi
 if [ "$2" != "" ]; then
     HEADPATH=$2
 else
-    HEADPATH='./git_version'
+    HEADPATH='./git_version.h'
 fi
 
 if [ "$3" != "" ]; then
@@ -49,9 +60,11 @@ echo "#if $PREFIX""_REPO_MODIFIED == 1" >> $GIT_HEADER
 echo "  #ifndef GITHEAD_NOWARNING" >> $GIT_HEADER
 echo "    #pragma message (\"******************************************\")" >> $GIT_HEADER
 echo "    #pragma message (\"*                                        *\")" >> $GIT_HEADER
-echo "    #pragma message (\"*      WARNING: repository modified      *\")" >> $GIT_HEADER
-echo "    #pragma message (\"*       changes not committed for        *\")" >> $GIT_HEADER
-echo "    #pragma message (\"*               $REPO_NAME      *\")" >> $GIT_HEADER
+centerName "WARNING: repository modified" >> $GIT_HEADER
+centerName "changes not committed for"  >> $GIT_HEADER
+#echo "    #pragma message (\"*      WARNING: repository modified      *\")" >> $GIT_HEADER
+#echo "    #pragma message (\"*       changes not committed for        *\")" >> $GIT_HEADER
+centerName $REPO_NAME >> $GIT_HEADER
 echo "    #pragma message (\"*                                        *\")" >> $GIT_HEADER
 echo "    #pragma message (\"******************************************\")" >> $GIT_HEADER
 echo "  #endif" >> $GIT_HEADER
