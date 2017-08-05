2dd64f9287fb86ae432364ea26628aafb0887bde
Jared <jaredmales@gmail.com>
2017-08-04 22:10:16 -0700

    updated templateBLAS no enums

--- include/math/templateBLAS.hpp
+++ include/math/templateBLAS.hpp
@@ -192,8 +192,13 @@ void hadd( const int N,
   * \ingroup template_blas
   */
 template<typename dataT>
-void gemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
+/*void gemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
           const enum CBLAS_TRANSPOSE TransB, const int M, const int N,
+          const int K, const dataT alpha, const dataT *A,
+          const int lda, const dataT *B, const int ldb,
+          const dataT beta, dataT *C, const int ldc)*/
+void gemm(const CBLAS_ORDER Order, const CBLAS_TRANSPOSE TransA,
+          const CBLAS_TRANSPOSE TransB, const int M, const int N,
           const int K, const dataT alpha, const dataT *A,
           const int lda, const dataT *B, const int ldb,
           const dataT beta, dataT *C, const int ldc)
@@ -206,8 +211,13 @@ void gemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
 
 template<>
 inline
-void gemm<float>(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
+/*void gemm<float>(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
                  const enum CBLAS_TRANSPOSE TransB, const int M, const int N,
+                 const int K, const float alpha, const float *A,
+                 const int lda, const float *B, const int ldb,
+                 const float beta, float *C, const int ldc)*/
+void gemm<float>(const CBLAS_ORDER Order, const CBLAS_TRANSPOSE TransA,
+                 const CBLAS_TRANSPOSE TransB, const int M, const int N,
                  const int K, const float alpha, const float *A,
                  const int lda, const float *B, const int ldb,
                  const float beta, float *C, const int ldc)
@@ -217,8 +227,13 @@ void gemm<float>(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA
 
 template<>
 inline
-void gemm<double>(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
+/*void gemm<double>(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
                   const enum CBLAS_TRANSPOSE TransB, const int M, const int N,
+                  const int K, const double alpha, const double *A,
+                  const int lda, const double *B, const int ldb,
+                  const double beta, double *C, const int ldc)*/
+void gemm<double>(const CBLAS_ORDER Order, const CBLAS_TRANSPOSE TransA,
+                  const CBLAS_TRANSPOSE TransB, const int M, const int N,
                   const int K, const double alpha, const double *A,
                   const int lda, const double *B, const int ldb,
                   const double beta, double *C, const int ldc)
@@ -229,8 +244,13 @@ void gemm<double>(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE Trans
 
 template<>
 inline
-void gemm<std::complex<float> >(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
+/*void gemm<std::complex<float> >(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
                                 const enum CBLAS_TRANSPOSE TransB, const int M, const int N,
+                                const int K, const std::complex<float> alpha, const std::complex<float> *A,
+                                const int lda, const std::complex<float> *B, const int ldb,
+                                const std::complex<float> beta, std::complex<float> *C, const int ldc)*/
+void gemm<std::complex<float> >(const CBLAS_ORDER Order, const CBLAS_TRANSPOSE TransA,
+                                const CBLAS_TRANSPOSE TransB, const int M, const int N,
                                 const int K, const std::complex<float> alpha, const std::complex<float> *A,
                                 const int lda, const std::complex<float> *B, const int ldb,
                                 const std::complex<float> beta, std::complex<float> *C, const int ldc)
@@ -240,8 +260,13 @@ void gemm<std::complex<float> >(const enum CBLAS_ORDER Order, const enum CBLAS_T
 
 template<>
 inline
-void gemm<std::complex<double> >(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
+/*void gemm<std::complex<double> >(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
                                  const enum CBLAS_TRANSPOSE TransB, const int M, const int N,
+                                 const int K, const std::complex<double> alpha, const std::complex<double> *A,
+                                 const int lda, const std::complex<double> *B, const int ldb,
+                                 const std::complex<double> beta, std::complex<double> *C, const int ldc)*/
+void gemm<std::complex<double> >(const CBLAS_ORDER Order, const CBLAS_TRANSPOSE TransA,
+                                 const CBLAS_TRANSPOSE TransB, const int M, const int N,
                                  const int K, const std::complex<double> alpha, const std::complex<double> *A,
                                  const int lda, const std::complex<double> *B, const int ldb,
                                  const std::complex<double> beta, std::complex<double> *C, const int ldc)
@@ -257,8 +282,12 @@ void gemm<std::complex<double> >(const enum CBLAS_ORDER Order, const enum CBLAS_
   * \ingroup template_blas
   */
 template<typename dataT>
-void syrk(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
+/*void syrk(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
           const enum CBLAS_TRANSPOSE Trans, const int N, const int K,
+          const dataT alpha, const dataT *A, const int lda,
+          const dataT beta, dataT *C, const int ldc)*/
+void syrk(const CBLAS_ORDER Order, const CBLAS_UPLO Uplo,
+          const CBLAS_TRANSPOSE Trans, const int N, const int K,
           const dataT alpha, const dataT *A, const int lda,
           const dataT beta, dataT *C, const int ldc)
 {
@@ -268,8 +297,12 @@ void syrk(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
 
 template<>
 inline
-void syrk<float>(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
+/*void syrk<float>(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
                  const enum CBLAS_TRANSPOSE Trans, const int N, const int K,
+                 const float alpha, const float *A, const int lda,
+                 const float beta, float *C, const int ldc)*/
+void syrk<float>(const CBLAS_ORDER Order, const CBLAS_UPLO Uplo,
+                 const CBLAS_TRANSPOSE Trans, const int N, const int K,
                  const float alpha, const float *A, const int lda,
                  const float beta, float *C, const int ldc)
 {
@@ -278,8 +311,12 @@ void syrk<float>(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
 
 template<>
 inline
-void syrk<double>(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
+/*void syrk<double>(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
                   const enum CBLAS_TRANSPOSE Trans, const int N, const int K,
+                  const double alpha, const double *A, const int lda,
+                  const double beta, double *C, const int ldc)*/
+void syrk<double>(const CBLAS_ORDER Order, const CBLAS_UPLO Uplo,
+                  const CBLAS_TRANSPOSE Trans, const int N, const int K,
                   const double alpha, const double *A, const int lda,
                   const double beta, double *C, const int ldc)
 {
@@ -288,8 +325,12 @@ void syrk<double>(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
 
 template<>
 inline
-void syrk<std::complex<float> >(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
+/*void syrk<std::complex<float> >(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
                                 const enum CBLAS_TRANSPOSE Trans, const int N, const int K,
+                                const std::complex<float> alpha, const std::complex<float> *A, const int lda,
+                                const std::complex<float> beta, std::complex<float> *C, const int ldc)*/
+void syrk<std::complex<float> >(const CBLAS_ORDER Order, const CBLAS_UPLO Uplo,
+                                const CBLAS_TRANSPOSE Trans, const int N, const int K,
                                 const std::complex<float> alpha, const std::complex<float> *A, const int lda,
                                 const std::complex<float> beta, std::complex<float> *C, const int ldc)
 {
@@ -298,8 +339,12 @@ void syrk<std::complex<float> >(const enum CBLAS_ORDER Order, const enum CBLAS_U
 
 template<>
 inline
-void syrk<std::complex<double> >(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
+/*void syrk<std::complex<double> >(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
                                  const enum CBLAS_TRANSPOSE Trans, const int N, const int K,
+                                 const std::complex<double> alpha, const std::complex<double> *A, const int lda,
+                                 const std::complex<double> beta, std::complex<double> *C, const int ldc)*/
+void syrk<std::complex<double> >(const CBLAS_ORDER Order, const CBLAS_UPLO Uplo,
+                                 const CBLAS_TRANSPOSE Trans, const int N, const int K,
                                  const std::complex<double> alpha, const std::complex<double> *A, const int lda,
                                  const std::complex<double> beta, std::complex<double> *C, const int ldc)
 {
