af43278d7cce67d73fd90039ba773338a5dc2499
Jared <jaredmales@gmail.com>
2017-08-06 17:12:31 -0700

    testing plan

--- include/fft/fft.hpp
+++ include/fft/fft.hpp
@@ -144,7 +144,7 @@ public:
       int pdir = FFTW_FORWARD;
       if(_dir == MXFFT_BACKWARD) pdir = FFTW_BACKWARD;
       
-      _plan = fftw_plan_dft<inputT, outputT>( fftwDimVec<dim>(_szX, _szY, _szZ), forplan1, forplan2,  pdir, FFTW_ESTIMATE);//FFTW_MEASURE);
+      _plan = fftw_plan_dft<inputT, outputT>( fftwDimVec<dim>(_szX, _szY, _szZ), forplan1, forplan2,  pdir, FFTW_MEASURE);
       
       fftw_free<inputT>(forplan1);
       fftw_free<outputT>(forplan2);
@@ -165,7 +165,7 @@ public:
       int pdir = FFTW_FORWARD;
       if(_dir == MXFFT_BACKWARD) pdir = FFTW_BACKWARD;
 
-      _plan = fftw_plan_dft<inputT, outputT>( fftwDimVec<dim>(_szX, _szY, _szZ),  reinterpret_cast<inputT*>(forplan), reinterpret_cast<outputT*>(forplan),  pdir, FFTW_ESTIMATE);//FFTW_MEASURE);
+      _plan = fftw_plan_dft<inputT, outputT>( fftwDimVec<dim>(_szX, _szY, _szZ),  reinterpret_cast<inputT*>(forplan), reinterpret_cast<outputT*>(forplan),  pdir, FFTW_MEASURE);
             
       fftw_free<inputT>(forplan);
    }
