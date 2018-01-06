a3ac6abb012329c8781d7f7bd5afc541de53ef25
Jared Males <jaredmales@gmail.com>
2018-01-05 17:05:48 -0700

    added copyright

--- include/wfp/idealCoronagraph.hpp
+++ include/wfp/idealCoronagraph.hpp
@@ -5,6 +5,25 @@
   *
   */
 
+//***********************************************************************//
+// Copyright 2015, 2016, 2017 Jared R. Males (jaredmales@gmail.com)
+//
+// This file is part of mxlib.
+//
+// mxlib is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// mxlib is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with mxlib.  If not, see <http://www.gnu.org/licenses/>.
+//***********************************************************************//
+
 #ifndef __idealCoronagraph_hpp__
 #define __idealCoronagraph_hpp__
 
