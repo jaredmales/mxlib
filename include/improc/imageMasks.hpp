ee3a1cc93dd91cfa39cd92d0f26e23fa25255fb1
Jared Males <jaredmales@gmail.com>
2018-01-13 23:22:12 -0700

    added license blurb

--- include/improc/imageMasks.hpp
+++ include/improc/imageMasks.hpp
@@ -4,7 +4,26 @@
   * \author Jared R. Males (jaredmales@gmail.com)
   *
   */
-   
+
+//***********************************************************************//
+// Copyright 2015, 2016, 2017 Jared R. Males (jaredmales@gmail.com)
+//
+// This file is part of mxlib.
+//
+// mxlib is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// mxlib is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with mxlib.  If not, see <http://www.gnu.org/licenses/>.
+//***********************************************************************//
+
 #ifndef improc_imageMasks_hpp
 #define improc_imageMasks_hpp
 
