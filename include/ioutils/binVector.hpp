baaada9072e39b3a1379bfe2a7d0890736c897bf
Jared Males <jaredmales@gmail.com>
2018-07-21 14:15:25 -0700

    add ioutils

---
+++ include/ioutils/binVector.hpp
@@ -0,0 +1,368 @@
+/** \file binVector.hpp
+  * \author Jared R. Males
+  * \brief A utility to read/write vectors of data from/to a binary file.
+  * \ingroup utils_files
+  */
+
+//***********************************************************************//
+// Copyright 2015, 2016, 2017 Jared R. Males (jaredmales@gmail.com)
+//
+// This file is part of mxlib.
+//
+// mxlib is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// mxlib is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with mxlib.  If not, see <http://www.gnu.org/licenses/>.
+//***********************************************************************//
+
+#ifndef binVector_hpp
+#define binVector_hpp
+
+
+#include <vector>
+
+#include "../mxError.hpp"
+
+namespace mx
+{
+namespace ioutils
+{
+
+/** \defgroup binvector binVector Binary File Format
+  * \ingroup ioutils
+  * \brief A simple binary file format for storing vectors of data on disk.
+  *
+  * The binVector file format is very simple: the first 8 bytes contain a unit64_t integer which specifies
+  * the type of the data. The second 8 bytes contain a uint64_t integer which specifies the length L of the data vector.
+  * The remaining L*sizeof(dataT) bytes contain the data.
+  *
+  * The suggested extension for BinVector files is ".binv".
+  *
+  *
+  */
+
+/// The type used for binVector sizes.
+/** \ingroup binVector
+  */
+typedef uint64_t binVSizeT;
+
+/// The type of binVector type codes.
+/** \ingroup binvector
+  */
+typedef uint64_t binVTypeT;
+
+///Namespace fo the bin-vector type-codes.
+/** \ingroup binvector
+  */
+namespace binVTypes
+{
+
+/// The pre-defined type codes for binVector.
+/** \ingroup binvector
+  */
+enum : binVTypeT { Bool = 0,
+                   SChar = 1,
+                   UChar = 2,
+                   Char = 3,
+                   WChar = 4,
+                   Char16 = 5,
+                   Char32 = 6,
+                   Int = 7,
+                   UInt = 8,
+                   SInt = 9,
+                   SUInt = 10,
+                   LInt = 11,
+                   LUInt = 12,
+                   LLInt = 13,
+                   LLUInt = 14,
+                   Float = 15,
+                   Double = 16,
+                   LDouble = 17
+                 };
+
+} //namespace binVTyes
+
+///Get the integer type code corresponding to the type.
+/**
+  * \returns an integer which uniquely identifies the type.
+  *
+  * \tparam dataT is the type
+  *
+  * \ingroup binvector
+  */
+template<typename dataT>
+binVTypeT binVectorTypeCode();
+
+template<>
+binVTypeT binVectorTypeCode<bool>()
+{
+   return binVTypes::Bool;
+}
+
+template<>
+binVTypeT binVectorTypeCode<signed char>()
+{
+   return binVTypes::SChar;
+}
+
+template<>
+binVTypeT binVectorTypeCode<unsigned char>()
+{
+   return binVTypes::UChar;
+}
+
+template<>
+binVTypeT binVectorTypeCode<char>()
+{
+   return binVTypes::Char;
+}
+
+template<>
+binVTypeT binVectorTypeCode<wchar_t>()
+{
+   return binVTypes::WChar;
+}
+
+template<>
+binVTypeT binVectorTypeCode<char16_t>()
+{
+   return binVTypes::Char16;
+}
+
+template<>
+binVTypeT binVectorTypeCode<char32_t>()
+{
+   return binVTypes::Char32;
+}
+
+template<>
+binVTypeT binVectorTypeCode<int>()
+{
+   return binVTypes::Int;
+}
+
+template<>
+binVTypeT binVectorTypeCode<unsigned int>()
+{
+   return binVTypes::UInt;
+}
+
+template<>
+binVTypeT binVectorTypeCode<short int>()
+{
+   return binVTypes::SInt;
+}
+
+template<>
+binVTypeT binVectorTypeCode<short unsigned int>()
+{
+   return binVTypes::SUInt;
+}
+
+template<>
+binVTypeT binVectorTypeCode<long int>()
+{
+   return binVTypes::LInt;
+}
+
+template<>
+binVTypeT binVectorTypeCode<long unsigned int>()
+{
+   return binVTypes::LUInt;
+}
+
+template<>
+binVTypeT binVectorTypeCode<long long int>()
+{
+   return binVTypes::LLInt;
+}
+
+template<>
+binVTypeT binVectorTypeCode<long long unsigned int>()
+{
+   return binVTypes::LLUInt;
+}
+
+
+template<>
+binVTypeT binVectorTypeCode<float>()
+{
+   return binVTypes::Float;
+}
+
+template<>
+binVTypeT binVectorTypeCode<double>()
+{
+   return binVTypes::Double;
+}
+
+template<>
+binVTypeT binVectorTypeCode<long double>()
+{
+   return binVTypes::LDouble;
+}
+
+
+/// Read a BinVector file from disk.
+/**
+  * \note dataT must match what was stored in the file.
+  *
+  * \returns 0 on success.
+  * \returns -1 if an error occurs.
+  *
+  * \ingroup binvector
+  */
+template<typename dataT>
+int readBinVector( std::vector<dataT> & vec, ///< [out] vec is a vector which will be resized and populated.
+                   const std::string & fname ///<  [in] fname is the name (full-path) of the file.
+                 )
+{
+   FILE *fin;
+   binVTypeT typecode;
+   binVSizeT sz;
+   int nrd;
+
+   fin = fopen(fname.c_str(), "r");
+   if(fin == 0)
+   {
+      mxPError("readBinVector", errno, "Error from fopen.");
+      return -1;
+   }
+
+   errno = 0;
+   nrd = fread(&typecode, sizeof(binVTypeT), 1, fin);
+
+   if(nrd != 1)
+   {
+      //Have to handle case where EOF reached but no error.
+      if(errno != 0)
+      {
+         mxPError("readBinVector", errno, "Error reading data size.");
+      }
+      else
+      {
+         mxError("readBinVector", MXE_FILERERR, "Error reading data size, did not read enough bytes.");
+      }
+      fclose(fin);
+      return -1;
+   }
+
+   if( typecode != binVectorTypeCode<dataT>() )
+   {
+      mxError("readBinVector", MXE_SIZEERR, "Mismatch between type dataT and type in file.");
+      fclose(fin);
+      return -1;
+   }
+
+
+   errno = 0;
+   nrd = fread(&sz, sizeof(binVSizeT), 1, fin);
+
+   if(nrd != 1)
+   {
+      //Have to handle case where EOF reached but no error.
+      if(errno != 0)
+      {
+         mxPError("readBinVector", errno, "Error reading vector size.");
+      }
+      else
+      {
+         mxError("readBinVector", MXE_FILERERR, "Error reading vector size, did not read enough bytes.");
+      }
+      fclose(fin);
+      return -1;
+   }
+
+   vec.resize(sz);
+
+   errno = 0;
+   nrd = fread(vec.data(), sizeof(dataT), sz, fin);
+
+   if(nrd != sz)
+   {
+      //Have to handle case where EOF reached but no error.
+      if(errno != 0)
+      {
+         mxPError("readBinVector", errno, "Error reading data.");
+      }
+      else
+      {
+         mxError("readBinVector", MXE_FILERERR, "Did not read enough data.");
+      }
+      fclose(fin);
+      return -1;
+   }
+
+   fclose(fin);
+
+   return 0;
+}
+
+/// Write a BinVector file to disk.
+/**
+  *
+  * \returns 0 on success.
+  * \returns -1 if an error occurs.
+  *
+  * \ingroup binvector
+  */
+template<typename dataT>
+int writeBinVector( const std::string & fname, ///< [in] fname is the name (full-path) of the file.
+                    std::vector<dataT> & vec ///< [in] vec is the vector which will be written to disk.
+                  )
+{
+
+   FILE *fout;
+   int nwr;
+   binVTypeT typecode = binVectorTypeCode<dataT>();
+   binVSizeT sz = vec.size();
+
+   fout = fopen(fname.c_str(), "wb");
+   if(fout == 0)
+   {
+      mxPError("writeBinVector", errno, "Error from fopen.");
+      return -1;
+   }
+
+   nwr = fwrite( &typecode, sizeof(binVTypeT), 1, fout);
+   if(nwr != 1)
+   {
+      mxPError("writeBinVector", errno, "Error writing typecode");
+      fclose(fout);
+      return -1;
+   }
+
+   nwr = fwrite( &sz, sizeof(binVSizeT), 1, fout);
+   if(nwr != 1)
+   {
+      mxPError("writeBinVector", errno, "Error writing vector size.");
+      fclose(fout);
+      return -1;
+   }
+
+   nwr = fwrite( vec.data(), sizeof(dataT), vec.size(), fout);
+   if(nwr != sz)
+   {
+      mxPError("writeBinVector", errno, "Error writing data.");
+      fclose(fout);
+      return -1;
+   }
+
+   fclose(fout);
+
+   return 0;
+}
+
+
+} //namespace ioutils
+} //namespace mx
+
+#endif //binVector_hpp
---
+++ include/ioutils/textTable.hpp
@@ -0,0 +1,193 @@
+/** \file textTable.hpp
+  * \brief declares and defines a simple text table manager
+  *
+  * \author Jared R. Males (jaredmales@gmail.com)
+  *
+  * \ingroup asciiutils
+  *
+  */
+
+//***********************************************************************//
+// Copyright 2015, 2016, 2017 Jared R. Males (jaredmales@gmail.com)
+//
+// This file is part of mxlib.
+//
+// mxlib is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// mxlib is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with mxlib.  If not, see <http://www.gnu.org/licenses/>.
+//***********************************************************************//
+
+#ifndef textTable_hpp
+#define textTable_hpp
+
+#include <string>
+#include <sstream>
+
+
+#include "stringUtils.hpp"
+
+/// The mxlib c++ namespace
+namespace mx
+{
+namespace ioutils
+{
+
+/** \addtogroup asciiutils
+  * @{
+  */
+
+///An ascii table formatter
+/** Manages a table of string content, including word-wrapping within cells.
+  * Contents are output to an ostream line by line with a table structure.
+  *
+  * \todo have ability to add whole row, with a variadic template to handle different types
+  * \todo allow non-wrapping (i.e. unlimited width) columns
+  * \todo headers and footers
+  * \todo add centering (h and v) and right and bottom justification
+  * \todo have a latex() setup function
+  * \todo add sort capability, with vector of column choices to sort first by column N, then M, etc.
+  */
+struct textTable
+{
+   std::vector<int> m_colWidths; ///< The widths of each column, not including the separator.
+
+   ///The table cells.
+   /** Organized as rows.cells.lines, where a cell could be multiple lines.
+     */
+   std::vector<std::vector<std::vector<std::string>>> m_rows;
+
+   std::string m_lineStart; ///< Text to print at the beginning of the line.
+
+   std::string m_lineEnd; ///< Text to print at the end of the line.
+
+   std::string m_colSep; ///< Text to print between each column.
+
+   std::string m_rowSep; ///< Text to print between each row.
+
+   ///Add one cell to the table, overwriting if it already exists.
+   void addCell( int row, ///< [in] the row of the cell.
+                 int col, ///< [in] the column of the cell.
+                 const std::string & cell  ///< [in] the new contents of the cell, will be wrapped.
+               );
+
+   void addCell( int row, ///< [in] the row of the cell.
+                 int col, ///< [in] the column of the cell.
+                 const char * cell  ///< [in] the new contents of the cell, will be wrapped.
+               );
+
+   ///Add one cell to the table, overwriting if it already exists.
+   template<typename typeT>
+   void addCell( int row, ///< [in] the row of the cell.
+                 int col, ///< [in] the column of the cell.
+                 const typeT & cell,  ///< [in] the new contents of the cell, will be wrapped.
+                 int precision=0
+               );
+
+   ///Output the table to a stream
+   /** Prints the table to the stream, with separators, etc.
+     *
+     * \tparam iosT is a std::ostream-like type.
+     */
+   template<typename iosT>
+   void outPut( iosT & ios /**< [in] a std::ostream-like stream.*/ );
+
+};
+
+inline
+void textTable::addCell( int row,
+                         int col,
+                         const std::string & cell
+                       )
+{
+   //Increase size if needed.
+   if( row >= m_rows.size() )
+   {
+      int N = m_rows.size();
+      for(int i=0; i < row - N +1; ++i)
+      {
+         m_rows.push_back( std::vector<std::vector<std::string>>( m_colWidths.size()));
+      }
+   }
+
+   m_rows[row][col].clear();
+   stringWrap(m_rows[row][col], cell, m_colWidths[col]);
+}
+
+void textTable::addCell(int row,
+                        int col,
+                        const char * cell
+                       )
+{
+   addCell(row, col, std::string(cell));
+}
+
+template<typename typeT>
+void textTable::addCell(int row,
+                        int col,
+                        const typeT & cell,
+                        int precision
+                       )
+{
+   addCell(row, col, convertToString<typeT>(cell, precision));
+}
+
+template<typename iosT>
+void textTable::outPut( iosT & ios )
+{
+   std::string line;
+
+   int width = 0;
+   for(int i=0;i<m_colWidths.size();++i) width += m_colWidths[i];
+   width += m_colWidths.size()*(m_colSep.length()-1); //+100;
+
+   line.resize(width, ' ');
+
+   for(int i=0; i< m_rows.size(); ++i)
+   {
+      int rowL = 0;
+      for(int j=0; j< m_colWidths.size(); ++j)
+      {
+         if( m_rows[i][j].size() > rowL ) rowL = m_rows[i][j].size();
+      }
+
+      for(int k=0; k< rowL; ++k)
+      {
+         //line.insert( 0, line.length(), ' ');
+         line.clear();
+         line.resize(width, ' ');
+         //line.replace(0, width, width, ' ');
+
+         int startPos = 0;
+         for(int j=0; j< m_colWidths.size(); ++j)
+         {
+            if(m_rows[i][j].size() > k) line.replace(startPos, m_rows[i][j][k].length(), m_rows[i][j][k]);
+            startPos += m_colWidths[j];
+
+            if( j < m_colWidths.size()-1) line.replace(startPos, m_colSep.length(), m_colSep);
+            startPos += m_colSep.length();
+
+         }
+
+         ios << m_lineStart << line << m_lineEnd << "\n";
+      }
+
+     if(m_rowSep.length() > 0) ios << m_rowSep << "\n";
+
+   }
+}
+
+/// @}
+
+} //namespace ioutils
+} //namespace mx
+
+#endif //textTable_hpp
