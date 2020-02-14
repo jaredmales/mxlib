801f6319cb00c90566143920aaf8eea0e6c1cb66
Jared Males <jaredmales@gmail.com>
2020-02-13 21:41:06 -0700

    wholePixel no longer a template paramter (which was dumb), switched to member m_ style, upated docs a little

--- include/wfp/fraunhoferPropagator.hpp
+++ include/wfp/fraunhoferPropagator.hpp
@@ -6,7 +6,7 @@
   */
 
 //***********************************************************************//
-// Copyright 2015, 2016, 2017 Jared R. Males (jaredmales@gmail.com)
+// Copyright 2015-2020 Jared R. Males (jaredmales@gmail.com)
 //
 // This file is part of mxlib.
 //
@@ -24,8 +24,8 @@
 // along with mxlib.  If not, see <http://www.gnu.org/licenses/>.
 //***********************************************************************//
 
-#ifndef __fraunhoferPropagator_hpp__
-#define __fraunhoferPropagator_hpp__
+#ifndef wfp_fraunhoferPropagator_hpp
+#define wfp_fraunhoferPropagator_hpp
 
 #include "../imagingArray.hpp"
 #include "imagingUtils.hpp"
@@ -46,10 +46,12 @@ namespace wfp
   * pupil to its original position.
   *
   * \tparam _wavefrontT is an Eigen::Array-like type, with std::complex values.
-  * \tparam wholePixel determines how the image is centered.  If 0 it is at 0.5*(wfSz-1), otherwise it is shifted by 0.5*wholePixel in each axis.
+  * 
+  * \todo check if we should reverse the FFT orders to be proper
+  * 
   * \ingroup imaging
   */
-template<typename _wavefrontT, int wholePixel=0>
+template<typename _wavefrontT>
 class fraunhoferPropagator
 {
 
@@ -67,19 +69,25 @@ public:
 protected:
 
    ///The size of the wavefront in pixels
-   int wavefrontSizePixels;
+   int m_wavefrontSizePixels {0};
 
-   realT xcen; ///<x-coordinate of focal plane center, in pixels
-   realT ycen; ///<x-coordinate of focal plane center, in pixels
+   realT m_xcen {0}; ///<x-coordinate of focal plane center, in pixels
+   realT m_ycen {0}; ///<x-coordinate of focal plane center, in pixels
 
+   /// Determines how the image is centered.  If 0 (default) it is at 0.5*(wfSz-1), if true it is shifted by 0.5*m_wholePixel in each axis.
+   realT m_wholePixel {0};
+   
    ///Phase screen for tilting the pupil plane so that the focal plane image is centered.
-   wavefrontT centerFocal;
+   wavefrontT m_centerFocal;
 
    ///Phase screen for un-tilting the pupil plane after propagating from a centered focal plane.
-   wavefrontT centerPupil;
+   wavefrontT m_centerPupil;
 
-   mx::fftT<complexT, complexT,2,0> fft_fwd;
-   mx::fftT<complexT, complexT,2,0> fft_back;
+   ///FFT object for forward FFTs
+   mx::fftT<complexT, complexT,2,0> m_fft_fwd;
+   
+   ///FFT object for backward FFTs
+   mx::fftT<complexT, complexT,2,0> m_fft_back;
 
    ///Initialize members
    void initialize();
@@ -93,11 +101,28 @@ public:
    ///Destructor
    ~fraunhoferPropagator();
 
-   ///Apply the shift to a pupil wavefront which will center the resultant focal plane image.
+   /// Get the value of the wholePixel parameter
+   /** The wholePixel parameter determines how the image is centered.  If 0 (default) it is at 0.5*(wfSz-1), if true it is shifted by 0.5*m_wholePixel in each axis.
+     * 
+     * \returns the value of m_wholePixel 
+     */
+   int wholePixel();
+   
+   /// Set the value of the wholePixel parameter
+   /** The wholePixel parameter determines how the image is centered.  If 0 (default) it is at 0.5*(wfSz-1), if true it is shifted by 0.5*m_wholePixel in each axis.
+     */
+   void wholePixel(realT wp /**< [in] the new wholePixel value */);
+   
+   ///Apply the shift to a pupil wavefront which will center the resultant focal plane image, and apply the normalization.
    /** You must have allocated the shift screens first, by calling propagatePupilToFocal, propagateFocalToPupil, or setWavefrontSizePixels.
      */
    void shiftPupil( wavefrontT & complexPupil /**< [in/out] the complex pupil plane wavefront to shift*/);
 
+   ///Apply the shift to a pupil wavefront which will restore it to a centered pupil image, with correct flux.
+   /** You must have allocated the shift screens first, by calling propagatePupilToFocal, propagateFocalToPupil, or setWavefrontSizePixels.
+     */
+   void unshiftPupil( wavefrontT & complexPupil /**< [in/out] the complex pupil plane wavefront to shift*/);
+   
    ///Propagate the wavefront from the pupil plane to the focal plane
    /** The pupil plane wavefront (complexPupil) is multiplied by a tilt to place the
      * image in the geometric center of the focal plane.
@@ -133,34 +158,55 @@ protected:
 
 };//class fraunhoferPropagator
 
-template<typename wavefrontT, int wholePixel>
-void fraunhoferPropagator<wavefrontT, wholePixel>::initialize()
+template<typename wavefrontT>
+void fraunhoferPropagator<wavefrontT>::initialize()
 {
-   wavefrontSizePixels = 0;
+   m_wavefrontSizePixels = 0;
 
-   xcen = 0;
-   ycen = 0;
+   m_xcen = 0;
+   m_ycen = 0;
 }
 
-template<typename wavefrontT, int wholePixel>
-fraunhoferPropagator<wavefrontT, wholePixel>::fraunhoferPropagator()
-{
-   initialize();
-}
-
-template<typename wavefrontT, int wholePixel>
-fraunhoferPropagator<wavefrontT, wholePixel>::~fraunhoferPropagator()
+template<typename wavefrontT>
+fraunhoferPropagator<wavefrontT>::fraunhoferPropagator()
 {
 }
 
-template<typename wavefrontT, int wholePixel>
-void fraunhoferPropagator<wavefrontT, wholePixel>::shiftPupil( wavefrontT & complexPupil )
+template<typename wavefrontT>
+fraunhoferPropagator<wavefrontT>::~fraunhoferPropagator()
 {
-   complexPupil *= centerFocal;
 }
 
-template<typename wavefrontT, int wholePixel>
-void fraunhoferPropagator<wavefrontT, wholePixel>::propagatePupilToFocal( wavefrontT & complexFocal,
+template<typename wavefrontT>
+int fraunhoferPropagator<wavefrontT>::wholePixel()
+{
+   return m_wholePixel;
+}
+   
+template<typename wavefrontT>
+void fraunhoferPropagator<wavefrontT>::wholePixel(realT wp)
+{
+   m_wholePixel = wp;
+   
+   //Re-make the shift phase if size already set.
+   if(m_wavefrontSizePixels > 0) makeShiftPhase();
+}
+   
+
+template<typename wavefrontT>
+void fraunhoferPropagator<wavefrontT>::shiftPupil( wavefrontT & complexPupil )
+{
+   complexPupil *= m_centerFocal;
+}
+
+template<typename wavefrontT>
+void fraunhoferPropagator<wavefrontT>::unshiftPupil( wavefrontT & complexPupil )
+{
+   complexPupil *= m_centerPupil;
+}
+
+template<typename wavefrontT>
+void fraunhoferPropagator<wavefrontT>::propagatePupilToFocal( wavefrontT & complexFocal,
                                                               wavefrontT & complexPupil
                                                             )
 {
@@ -168,65 +214,65 @@ void fraunhoferPropagator<wavefrontT, wholePixel>::propagatePupilToFocal( wavefr
    setWavefrontSizePixels(complexPupil.rows());
 
    //Apply the centering shift -- this adjusts by 0.5 pixels and normalizes
-   complexPupil *= centerFocal;
-
+   shiftPupil(complexPupil);
+   
    //fft_fwd.fft(complexPupil.data(), complexFocal.data() );
-   fft_fwd( complexFocal.data(), complexPupil.data() );
+   m_fft_fwd( complexFocal.data(), complexPupil.data() );
 }
 
-template<typename wavefrontT, int wholePixel>
-void fraunhoferPropagator<wavefrontT, wholePixel>::propagateFocalToPupil(wavefrontT & complexPupil, wavefrontT & complexFocal)
+template<typename wavefrontT>
+void fraunhoferPropagator<wavefrontT>::propagateFocalToPupil(wavefrontT & complexPupil, wavefrontT & complexFocal)
 {
    //First setup the tilt screens (does nothing if there's no change in size)
    setWavefrontSizePixels(complexPupil.rows());
 
    //fft_back.fft( complexFocal.data(), complexPupil.data());
-   fft_back( complexPupil.data(), complexFocal.data() );
+   m_fft_back( complexPupil.data(), complexFocal.data() );
 
    //Unshift the wavefront and normalize
-   complexPupil *= centerPupil;
+   unshiftPupil(complexPupil);
 }
 
-template<typename wavefrontT, int wholePixel>
-void fraunhoferPropagator<wavefrontT, wholePixel>::setWavefrontSizePixels(int wfsPix)
+template<typename wavefrontT>
+void fraunhoferPropagator<wavefrontT>::setWavefrontSizePixels(int wfsPix)
 {
    //If no change in size, do nothing
-   if(wfsPix == centerFocal.rows()) return;
+   if(wfsPix == m_centerFocal.rows()) return;
 
-   wavefrontSizePixels = wfsPix;
+   m_wavefrontSizePixels = wfsPix;
 
-   xcen = 0.5*(wfsPix - 1.0);
-   ycen = 0.5*(wfsPix - 1.0);
+   m_xcen = 0.5*(wfsPix - 1.0);
+   m_ycen = 0.5*(wfsPix - 1.0);
 
    makeShiftPhase();
 
-   fft_fwd.plan(wfsPix, wfsPix);
+   m_fft_fwd.plan(wfsPix, wfsPix);
 
-   fft_back.plan(wfsPix, wfsPix, MXFFT_BACKWARD);
+   m_fft_back.plan(wfsPix, wfsPix, MXFFT_BACKWARD);
 }
 
-template<typename wavefrontT, int wholePixel>
-void fraunhoferPropagator<wavefrontT, wholePixel>::makeShiftPhase()
+template<typename wavefrontT>
+void fraunhoferPropagator<wavefrontT>::makeShiftPhase()
 {
    realT pi = boost::math::constants::pi<realT>();
 
    //The normalization is included in the tilt.
-   realT norm = 1./(wavefrontSizePixels*sqrt(2));
+   realT norm = 1./(m_wavefrontSizePixels*sqrt(2));
    complexT cnorm = complexT(norm, norm);
 
    //Resize the center phases
-   centerFocal.resize(wavefrontSizePixels, wavefrontSizePixels);
-   centerPupil.resize(wavefrontSizePixels, wavefrontSizePixels);
+   m_centerFocal.resize(m_wavefrontSizePixels, m_wavefrontSizePixels);
+   m_centerPupil.resize(m_wavefrontSizePixels, m_wavefrontSizePixels);
 
    //Shift by 0.5 pixels
-   realT arg = -2.0*pi*0.5*(wavefrontSizePixels-wholePixel)/(wavefrontSizePixels-1);
+   realT arg = -2.0*pi*0.5*(m_wavefrontSizePixels-m_wholePixel)/(m_wavefrontSizePixels-1);
 
-   for(int ii=0; ii < wavefrontSizePixels; ++ii)
+   for(int ii=0; ii < m_wavefrontSizePixels; ++ii)
    {
-      for(int jj=0; jj < wavefrontSizePixels; ++jj)
+      for(int jj=0; jj < m_wavefrontSizePixels; ++jj)
       {
-         centerFocal(ii,jj) = cnorm*exp(complexT(0.,arg*((ii-xcen)+(jj-ycen))));
-         centerPupil(ii,jj) = cnorm*exp(complexT(0., 0.5*pi - arg*((ii-xcen)+(jj-ycen))));
+         m_centerFocal(ii,jj) = cnorm*exp(complexT(0.,arg*((ii-m_xcen)+(jj-m_ycen))));
+         m_centerPupil(ii,jj) = cnorm*exp(complexT(0., 0.5*pi - arg*((ii-m_xcen)+(jj-m_ycen))));
       }
    }
 }
@@ -234,4 +280,4 @@ void fraunhoferPropagator<wavefrontT, wholePixel>::makeShiftPhase()
 } //namespace wfp
 } //namespace mx
 
-#endif //__fraunhoferPropagator_hpp__
+#endif //wfp_fraunhoferPropagator_hpp
