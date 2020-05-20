766d5424a88609156c28a188a6833940be46e0c2
Jared Males <jaredmales@gmail.com>
2020-05-19 18:41:52 -0700

    added postMedSub correctly

--- include/improc/KLIPreduction.hpp
+++ include/improc/KLIPreduction.hpp
@@ -594,6 +594,24 @@ int KLIPreduction<_realT, _derotFunctObj, _evCalcT>::regions( std::vector<_realT
    
    if(this->m_postMedSub)
    {
+      std::cerr << "Subtracting medians in post\n";
+      
+   for(size_t n=0; n<this->m_psfsub.size(); ++n)
+   {
+      #pragma omp parallel
+      {
+         eigenImage<realT> medim;
+         
+         this->m_psfsub[n].median(medim);
+         
+         #pragma omp for
+         for(int i=0; i<this->m_psfsub[n].planes();++i)
+         {
+            this->m_psfsub[n].image(i) -= medim;
+         }
+      }
+   }
+   
       eigenImage<realT> medim;
       this->m_tgtIms.median(medim);
       for(int p=0;p<this->m_tgtIms.planes();++p) this->m_tgtIms.image(p) -= medim;
