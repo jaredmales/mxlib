d024ab978c5d750f41027547f661a0594d5ba55b
Jared Males <jaredmales@gmail.com>
2021-05-14 23:28:00 -0700

    added GIT_PATH option for app versioning

--- mk/MxApp.mk
+++ mk/MxApp.mk
@@ -21,6 +21,7 @@ ifeq ($(GIT_VERSION),yes)
    GIT_VERSION_DEF = $(shell echo $(TARGET) | tr a-z A-Z)_GIT
    PRE_TARGETS += git_version
    GIT_VERSION_FILE = $(TARGETNAME)_git_version.h
+   GIT_PATH ?= ./
 endif
 
 
@@ -41,7 +42,7 @@ tshoot:
 
 .PHONY: git_version
 git_version:
-	@bash gengithead.sh ./ $(TARGETNAME)_git_version.h $(GIT_VERSION_DEF)
+	@bash gengithead.sh $(GIT_PATH) $(TARGETNAME)_git_version.h $(GIT_VERSION_DEF)
 	
 .PHONY: clean
 clean:
