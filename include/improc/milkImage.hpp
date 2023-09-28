e84e54aafc7878090d34a769c3e06933a956e952
Jared Males <jaredmales@pm.me>
2023-09-27 19:52:01 -0700

    updated milkImage

--- include/improc/milkImage.hpp
+++ include/improc/milkImage.hpp
@@ -42,6 +42,86 @@ namespace mx
 namespace improc 
 {
 
+template<typename typeT>
+struct ImageStructTypeCode;
+
+template<>
+struct ImageStructTypeCode<uint8_t>
+{
+    constexpr static int TypeCode = _DATATYPE_UINT8;
+};
+
+template<>
+struct ImageStructTypeCode<int8_t>
+{
+    constexpr static int TypeCode = _DATATYPE_INT8;
+};
+
+template<>
+struct ImageStructTypeCode<char>
+{
+    constexpr static int TypeCode = _DATATYPE_INT8;
+};
+
+template<>
+struct ImageStructTypeCode<uint16_t>
+{
+    constexpr static int TypeCode = _DATATYPE_UINT16;
+};
+
+template<>
+struct ImageStructTypeCode<int16_t>
+{
+    constexpr static int TypeCode = _DATATYPE_INT16;
+};
+
+template<>
+struct ImageStructTypeCode<uint32_t>
+{
+    constexpr static int TypeCode = _DATATYPE_UINT32;
+};
+
+template<>
+struct ImageStructTypeCode<int32_t>
+{
+    constexpr static int TypeCode = _DATATYPE_INT32;
+};
+
+template<>
+struct ImageStructTypeCode<uint64_t>
+{
+    constexpr static int TypeCode = _DATATYPE_UINT64;
+};
+template<>
+struct ImageStructTypeCode<int64_t>
+{
+    constexpr static int TypeCode = _DATATYPE_INT64;
+};
+template<>
+struct ImageStructTypeCode<float>
+{
+    constexpr static int TypeCode = _DATATYPE_FLOAT;
+};
+
+template<>
+struct ImageStructTypeCode<double>
+{
+    constexpr static int TypeCode = _DATATYPE_DOUBLE;
+};
+
+template<>
+struct ImageStructTypeCode<std::complex<float>>
+{
+    constexpr static int TypeCode = _DATATYPE_COMPLEX_FLOAT;
+};
+
+template<>
+struct ImageStructTypeCode<std::complex<double>>
+{
+    constexpr static int TypeCode = _DATATYPE_COMPLEX_DOUBLE;
+};
+
+
 template<typename _dataT>
 class milkImage
 {
@@ -49,48 +129,103 @@ public:
 
     typedef _dataT dataT;
 
+protected:
+
+    std::string m_name; ///< The image name, from name.im.shm (the .im.shm is silent). 
+
     IMAGE * m_image {nullptr};
 
     eigenImageMap<dataT> * m_map {nullptr};
 
-    void open( const std::string & imname )
-    {
-        if(m_image)
-        {
-            ImageStreamIO_closeIm(m_image);
-            delete m_image;
-            m_image = nullptr;
-        }
+public:
 
-        m_image = new IMAGE;
+    milkImage();
 
-        ImageStreamIO_openIm(m_image, imname.c_str());
+    milkImage( const std::string & imname );
 
-        if(m_map)
-        {
-            delete m_map;
-            m_map = nullptr;
-        }
+    void open( const std::string & imname );
 
-        m_map = new eigenImageMap<dataT>((dataT*)m_image->array.raw, m_image->md->size[0], m_image->md->size[1]);
-    }
+    void reopen();
 
-    eigenImageMap<dataT> & image() {return *m_map;}
-    
-    eigenImageMap<dataT> * operator->() {return m_map;}
+    void close();
 
-    operator eigenImageMap<dataT>&() {return *m_map;}
+    eigenImageMap<dataT> & map(); 
 
+    eigenImageMap<dataT> * operator->();
 
+    operator eigenImageMap<dataT>&();
 
-    void post()
-    {
-        if(!m_image) return;
+    void post();
 
-        ImageStreamIO_UpdateIm(m_image);
-    }
 };
 
+template<typename dataT>
+milkImage<dataT>::milkImage()
+{
+}
+
+template<typename dataT>
+milkImage<dataT>::milkImage( const std::string & imname )
+{
+    open(imname);
+}
+
+template<typename dataT>
+void milkImage<dataT>::open( const std::string & imname )
+{
+    if(m_image)
+    {
+        ImageStreamIO_closeIm(m_image);
+        delete m_image;
+        m_image = nullptr;
+    }
+    
+    m_image = new IMAGE;
+    
+    ImageStreamIO_openIm(m_image, imname.c_str());
+     
+    if(ImageStructTypeCode<dataT>::TypeCode != m_image->md->datatype)
+    {
+        delete m_image;
+        m_image = nullptr;
+        throw std::invalid_argument("shmim datatype does not match template type");
+    }
+
+    m_name = imname;
+
+    if(m_map)
+    {
+        delete m_map;
+        m_map = nullptr;
+    }
+
+    m_map = new eigenImageMap<dataT>((dataT*)m_image->array.raw, m_image->md->size[0], m_image->md->size[1]);
+}
+
+template<typename dataT>
+eigenImageMap<dataT> & milkImage<dataT>::map() 
+{
+    return *m_map;
+}
+
+template<typename dataT>
+eigenImageMap<dataT> * milkImage<dataT>::operator->() 
+{
+    return m_map;
+}
+
+template<typename dataT>
+milkImage<dataT>::operator eigenImageMap<dataT>&()
+{
+    return *m_map;
+}
+
+template<typename dataT>
+void milkImage<dataT>::post()
+{
+    if(!m_image) return;
+    ImageStreamIO_UpdateIm(m_image);
+}
 
 }
 }
