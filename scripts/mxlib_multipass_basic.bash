b8cb72bfe3bb783f6107e9e43649ef7c539b6e90
Jared Males <jaredmales@gmail.com>
2025-07-24 20:38:47 -0700

    fixed missing lapacke

--- scripts/mxlib_multipass_basic.bash
+++ scripts/mxlib_multipass_basic.bash
@@ -14,7 +14,6 @@
 
 #basic VM creation
 multipass launch -n mxlib-vm -c 4 -d 20GiB -m 8.0GiB 24.04
-multipass mount $HOME mxlib-vm:/home/ubuntu/Home
 
 #install our key
 multipass exec mxlib-vm -- bash -c "echo `cat ~/.ssh/id_ed25519.pub` >> ~/.ssh/authorized_keys"
@@ -28,4 +27,4 @@ multipass stop mxlib-vm
 multipass start mxlib-vm
 
 #get MagAO-X repo
-ssh ubuntu@$(multipass exec mxlib-vm -- hostname -I | awk '{ print $1 }' ) "git clone --depth=1 https://github.com/jaredmales/mxlib.git"
+ssh ubuntu@$(multipass exec mxlib-vm -- hostname -I | awk '{ print $1 }' ) "mkdir Source;cd Source;git clone --depth=1 https://github.com/jaredmales/mxlib.git"
--- scripts/mxlib_provision_ubuntu2404.sh
+++ scripts/mxlib_provision_ubuntu2404.sh
@@ -11,68 +11,41 @@ set -e
 # Switches some repos to dev TODO: make this configurable
 #################################################################################
 
-# from https://github.com/milk-org/milk/blob/dev/Dockerfile
 sudo apt-get install -y \
     git \
     make \
-    dpkg-dev \
-    libc6-dev \
     cmake \
     pkg-config \
-    python3-dev \
-    libcfitsio-dev \
-    pybind11-dev \
-    python3-pybind11 \
-    libgsl-dev \
-    libfftw3-dev \
-    libncurses-dev \
-    libbison-dev \
-    libfl-dev \
-    libreadline-dev \
-    gfortran libopenblas-dev liblapacke-dev \
-    pkg-config \
     gcc \
-    g++
-
+    g++ \
+    libgsl-dev \
+    libboost-all-dev \
+    libcfitsio-dev \
+    libopenblas-dev \
+    liblapacke-dev \
+    libfftw3-dev \
+    libeigen3-dev
 
 ## Make work directory
-mkdir -p ~/Source
-cd ~/Source
+mkdir -p $HOME/Source
 
-## Install milk
-if ! command -v milk; then
-    if [[ ! -d milk ]]; then
-        git clone --recursive https://github.com/milk-org/milk.git
-    fi
-    cd milk
-    git checkout dev
-    mkdir -p _build
-    cd _build
-    cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local
-    sudo make install
-    if [[ ! -h /usr/local/milk ]]; then
-        sudo ln -sfv /usr/local/milk-* /usr/local/milk
-    fi
-fi
-
-## Setup milk for linking
-if [[ ! -z /etc/ld.so.conf.d/milk.conf ]]; then
-    echo /usr/local/milk/lib/ | sudo tee -a  /etc/ld.so.conf.d/milk.conf
-    sudo ldconfig
-fi
 
 ## mxlib
-cd ~/Source
-sudo apt-get install -y libgsl-dev libboost-all-dev libcfitsio-dev libopenblas-dev libfftw3-dev libeigen3-dev
+cd $HOME/Source
 if [[ ! -d mxlib ]]; then
     git clone https://github.com/jaredmales/mxlib.git
 fi
 cd mxlib
 git checkout dev
-if [[ ! -z local/Common.mk ]]; then
-    echo NEED_CUDA=no > local/Common.mk
-    echo PREFIX=/usr/local >> local/Common.mk
-fi
+mkdir -p _build
+cd _build
+cmake ..
 make
 sudo make install
+
+
+
+
+
+
 
