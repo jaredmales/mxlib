84392f034a4d6c3ba81d35bcaf52161e14ae1a58
Jared Males <jaredmales@gmail.com>
2026-06-09 22:20:23 -0700

    install on ubuntu 26; updated Boost search in cmake

---
+++ scripts/milk.sh
@@ -0,0 +1,8 @@
+#Normally copy this to /etc/profile.d after installing milk and symlinking /usr/local/milk
+
+export PATH="$PATH:/usr/local/milk/bin"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/milk/lib/pkgconfig
+export MILK_SHM_DIR=/milk/shm
+export MILK_ROOT=/opt/MagAOX/source/milk
+export MILK_INSTALLDIR=/usr/local/milk
+
