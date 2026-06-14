ca054f7fa7d17fae1f619ef3e8d4c6bb62ead154
Jared Males <jaredmales@gmail.com>
2026-06-14 15:04:29 -0600

    updated for install on U26

--- scripts/milk.sh
+++ scripts/milk.sh
@@ -1,3 +1,5 @@
+#symlink milk
+
 #Normally copy this to /etc/profile.d after installing milk and symlinking /usr/local/milk
 
 export PATH="$PATH:/usr/local/milk/bin"
@@ -6,3 +8,5 @@ export MILK_SHM_DIR=/milk/shm
 export MILK_ROOT=/opt/MagAOX/source/milk
 export MILK_INSTALLDIR=/usr/local/milk
 
+#/etc/ld.so.conf.d/milk.conf 
+/usr/local/milk/lib/
